acf81da1ec802bc7e27512420d102bc98d1a1877
Enric Vergara <donkikochan@gmail.com>
2024-09-25 12:39:08 +0200

    gestió de click sobre el token

--- Assets/Scripts/Token.cs
+++ Assets/Scripts/Token.cs
@@ -1,19 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Token : MonoBehaviour
 {
+    private GameObject gm;
     public GameObject sphere;
+    
     // Start is called before the first frame update
     void Start()
     {
         sphere.SetActive(false);
+        
+        gm = GameObject.FindGameObjectWithTag("GameController");
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (sphere.activeSelf)
+        {
+            Debug.Log("Han fet click sobre el talp");    
+        }
         
     }
 

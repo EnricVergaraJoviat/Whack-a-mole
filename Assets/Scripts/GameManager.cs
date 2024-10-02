62545dce1a85d1abeb0f969bc2eac1561990d625
Enric Vergara <donkikochan@gmail.com>
2024-10-02 12:47:12 +0200

    ...

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,14 +7,22 @@ using Random = System.Random;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Time Settings:")] 
+    public float minTimeToAppear;
+    public float maxTimeToAppear;
+    public int minNumberOfMoles;
+    public int maxNumberOfMoles;
+    public float totalTime;
+    public GameObject[] tokens;
+    
+    [Header("UI Settings:")]
     public TextMeshProUGUI lbl_Score;
     public TextMeshProUGUI lbl_Countdown;
     public TextMeshProUGUI lbl_GameFinished;
     
     
-    public GameObject[] tokens;
+    
     private int score;
-    private float timer;
     private bool gameFinished = false;
     
     // Start is called before the first frame update
@@ -22,10 +30,12 @@ public class GameManager : MonoBehaviour
     {
         gameFinished = false;
         score = 0;
-        timer = 4.0f;
+        
         lbl_Score.text = "Score: 0";
-        lbl_Countdown.text = "Time: " + timer;
+        lbl_Countdown.text = "Time: " + totalTime;
         lbl_GameFinished.text = "";
+        
+        Invoke("ActivateMoles", minTimeToAppear);
     }
 
 
@@ -35,23 +45,10 @@ public class GameManager : MonoBehaviour
         {
             return;
         }
-        
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            int index = UnityEngine.Random.Range(0, 25);
-            //Token t = tokens[0].GetComponent<Token>().ActivateToken(true);
-            Token t = tokens[index].GetComponent<Token>();
-            bool isRed = true;
-            if (UnityEngine.Random.value <= 0.5f)
-            {
-                isRed = false;    
-            }
-            t.ActivateToken(isRed);
-        }
 
-        timer -= Time.deltaTime;
-        lbl_Countdown.text = "Time: " + (int)timer;
-        if (timer <= 0.0f)
+        totalTime -= Time.deltaTime;
+        lbl_Countdown.text = "Time: " + (int)totalTime;
+        if (totalTime <= 0.0f)
         {
             lbl_GameFinished.text = "Game Finished";
             gameFinished = true;
@@ -67,5 +64,31 @@ public class GameManager : MonoBehaviour
     {
         score += value;
         lbl_Score.text = "Score: " + score;
+    }
+
+    private void ActivateMoles()
+    {
+        
+        int numOfMolesToBeActivated = UnityEngine.Random.Range(minNumberOfMoles, maxNumberOfMoles+1);
+        for (int i = 0; i < numOfMolesToBeActivated; i++)
+        {
+            int index = UnityEngine.Random.Range(0, 25);
+            //Token t = tokens[0].GetComponent<Token>().ActivateToken(true);
+            Token t = tokens[index].GetComponent<Token>();
+            bool isRed = true;
+            if (UnityEngine.Random.value <= 0.5f)
+            {
+                isRed = false;    
+            }
+            t.ActivateToken(isRed);
+        }
+        
+
+        if (!gameFinished)
+        {
+            float time = UnityEngine.Random.Range(minTimeToAppear, maxTimeToAppear);
+            Invoke("ActivateMoles", time);    
+        }
+        
     }
 }
